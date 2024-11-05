53fbc3a9cc968928bac63f0144ed20ea7c9f5685
Rayhanica <rayhanusmanu1@gmail.com>
2024-11-05 12:11:19 -0500

    My new Project

---
+++ nextjs-dashboard/nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Inter, Lusitana } from 'next/font/google';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+       <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
